04782eef5ad9804e8a266f4b495e932853837543
陈安一 <baozi@chenanyi.com>
2017-01-12 02:06:54 +0800

    [+]接入极光推送SDK，实现消息推送

---
+++ src/main/java/com/cay/Helper/PushHelper.java
@@ -0,0 +1,44 @@
+package com.cay.Helper;
+
+import cn.jpush.api.push.model.Platform;
+import cn.jpush.api.push.model.PushPayload;
+import cn.jpush.api.push.model.audience.Audience;
+import cn.jpush.api.push.model.notification.Notification;
+
+public class PushHelper {
+	private static String SEND_MSG="";
+	
+	/*
+	 * 快捷地构建推送对象：所有平台，所有设备，内容为 SEND_MSG 的通知。
+	 */
+	public static PushPayload PushAll(String msg) {
+        return PushPayload.alertAll(msg);
+    }
+	
+	/**
+	 * 构建推送对象：所有平台，推送目标是别名为 "target"，通知内容为 SEND_MSG
+	 * @Param: target 推送目标
+	 * @Param: msg 内容
+	 */
+	public static PushPayload PushTarget(String target, String msg) {
+        return PushPayload.newBuilder()
+                .setPlatform(Platform.all())
+                .setAudience(Audience.alias(target))
+                .setNotification(Notification.alert(msg))
+                .build();
+    }
+	
+	/**
+	 *@Param: plantform 发送平台 枚举值
+	 *@Param: target 推送目标
+	 *@Param: title 标题
+	 *@Param: msg 内容
+	 */
+	public static PushPayload PushAndroidWithTitle(Platform plantform, String msg, String target, String title) {
+        return PushPayload.newBuilder()
+                .setPlatform(plantform)
+                .setAudience(Audience.tag(target))
+                .setNotification(Notification.android(msg, title, null))
+                .build();
+    }
+}
---
+++ src/main/java/com/cay/Model/Config/PushConfig.java
@@ -0,0 +1,26 @@
+package com.cay.Model.Config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by 陈安一 on 2017/1/11.
+ */
+@Component
+@ConfigurationProperties(prefix = "push")
+public class PushConfig {
+    private String appKey;
+	private String masterSecret;
+    public String getAppKey() {
+		return appKey;
+	}
+	public void setAppKey(String appKey) {
+		this.appKey = appKey;
+	}
+	public String getMasterSecret() {
+		return masterSecret;
+	}
+	public void setMasterSecret(String masterSecret) {
+		this.masterSecret = masterSecret;
+	}
+}
